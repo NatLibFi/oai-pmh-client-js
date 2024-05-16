8e35b8e3bc9a3329cec9b3c46e3068ec66fdec6d
Minttu Hurme <ammsalme@users.noreply.github.com>
2024-05-16 18:19:07 +0300

    Dependency & Maintenance update (#55)
    * Bump the development-dependencies group with 1 update (#31)
    
    * Update deps
    * Update node-tests: v16.x -> v20.x
    * Update copyright year
    
    
    * Bump the production-dependencies group with 1 update
    Bumps the production-dependencies group with 1 update: [moment](https://github.com/moment/moment).
    
    * Bump the development-dependencies group with 6 updates
    
    | Package | From | To |
    | --- | --- | --- |
    | [@babel/core](https://github.com/babel/babel/tree/HEAD/packages/babel-core) | `7.23.6` | `7.23.7` |
    | [@babel/preset-env](https://github.com/babel/babel/tree/HEAD/packages/babel-preset-env) | `7.23.6` | `7.23.8` |
    | [@babel/register](https://github.com/babel/babel/tree/HEAD/packages/babel-register) | `7.22.15` | `7.23.7` |
    | [@natlibfi/fixugen-http-client](https://github.com/natlibfi/fixugen-http-client-js) | `3.0.3` | `3.0.4` |
    | [chai](https://github.com/chaijs/chai) | `4.3.10` | `4.4.1` |
    | [nodemon](https://github.com/remy/nodemon) | `3.0.2` | `3.0.3` |
    * Bump mikaelvesavuori/license-compliance-action from 1.0.2 to 1.0.3 (#45)
    
    Bumps [mikaelvesavuori/license-compliance-action](https://github.com/mikaelvesavuori/license-compliance-action) from 1.0.2 to 1.0.3.
    
    * Bump the production-dependencies group with 1 update (#47)
    Bumps the production-dependencies group with 1 update: [http-status](https://github.com/adaltas/node-http-status).
    
    * Bump the development-dependencies group with 8 updates (#54)
    
    | Package | From | To |
    | --- | --- | --- |
    | [@babel/cli](https://github.com/babel/babel/tree/HEAD/packages/babel-cli) | `7.23.4` | `7.24.5` |
    | [@babel/core](https://github.com/babel/babel/tree/HEAD/packages/babel-core) | `7.23.7` | `7.24.5` |
    | [@babel/node](https://github.com/babel/babel/tree/HEAD/packages/babel-node) | `7.22.19` | `7.23.9` |
    | [@babel/preset-env](https://github.com/babel/babel/tree/HEAD/packages/babel-preset-env) | `7.23.8` | `7.24.5` |
    | [@natlibfi/eslint-config-melinda-backend](https://github.com/natlibfi/eslint-config-melinda-backend) | `3.0.3` | `3.0.4` |
    | [eslint](https://github.com/eslint/eslint) | `8.56.0` | `8.57.0` |
    | [mocha](https://github.com/mochajs/mocha) | `10.2.0` | `10.4.0` |
    | [nodemon](https://github.com/remy/nodemon) | `3.0.3` | `3.1.0` |
    
    
    * Update deps
    
    * Update copyright year
    
    * Update license to MIT [MRA-40]
    
    * Add node-tests: v21, v22
    
    * 3.0.6-alpha.1
    
    * Bump the development-dependencies group across 1 directory with 3 updates (#57)
    
    
    * 3.0.6-alpha.2

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,31 +1,3 @@
-/**
-*
-* @licstart  The following is the entire license notice for the JavaScript code in this file.
-*
-* OAI-PMH Javascript client library
-*
-* Copyright (C) 2020 University Of Helsinki (The National Library Of Finland)
-*
-* This file is part of oai-pmh-client-js
-*
-* oai-pmh-client-js program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* oai-pmh-client-js is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-* @licend  The above is the entire license notice
-* for the JavaScript code in this file.
-*
-*/
-
 import createClient, {OaiPmhError} from '.';
 import {expect} from 'chai';
 import {READERS} from '@natlibfi/fixura';
